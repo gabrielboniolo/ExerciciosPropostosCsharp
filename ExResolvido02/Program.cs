2dae7338bea2b30731a0dca6d13ef0c4b8d562eb
Gabriel Boniolo <gabrielboniolo@gmail.com>
2023-04-13 20:21:54 -0300

    d02

---
+++ ExResolvido02/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ExResolvido02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, alt, area, per, diag;
+            b = double.Parse(Console.ReadLine());
+            alt = double.Parse(Console.ReadLine());
+            area = b * alt;
+            per = (b + alt) * 2;
+            diag = Math.Sqrt(Math.Pow(b,2) + Math.Pow(alt,2));
+            Console.WriteLine("AREA = {0}", area.ToString("F4"));
+            Console.WriteLine("PERÍMETRO = {0}", per.ToString("F4"));
+            Console.WriteLine("DIAGONAL = {0}", diag.ToString("F4"));
+
+
+        }
+    }
+}
---
+++ ExResolvido03/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExResolvido03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome1, nome2;
+            int idade1, idade2;
+            string[] vetor = Console.ReadLine().Split(' ');
+            nome1= vetor[0];
+            idade1 = int.Parse(vetor[1]);
+            nome2= vetor[2];
+            idade2 = int.Parse(vetor[3]);
+            double media = (idade1 + idade2) / 2;
+            Console.WriteLine("As pessoas {0} e {1} possuem uma média de idade de {2} anos", nome1, nome2, media);
+        }
+    }
+}
